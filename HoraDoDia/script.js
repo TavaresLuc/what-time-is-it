1805e5d02d564139efa6826778b1d3753fe6eea5
Lucas Tavares <tavaresluc@gmail.com>
2022-04-05 23:14:51 -0300

    responsive changes

--- HoraDoDia/script.js
+++ HoraDoDia/script.js
@@ -1,42 +1,62 @@
-function atualizar() {
-var mensagem = window.document.getElementById('div1')
-var imagem = window.document.querySelector('img#imagens')
-var titulo = document.querySelector('header#cab')
-var data = new Date()
-var hora = data.getHours()
-var hora2 = data.getMinutes()
-var hora3 = data.getSeconds()
+const hour = new Date()
+const hora = hour.getHours()
+const min = hour.getMinutes()
+const sec = hour.getUTCSeconds()
 
-var myVar = setInterval(myTimer ,1000);
+var msg = document.getElementById('hour')
+var picture = document.getElementById('img')
+
+msg.innerHTML = `Welcome! You visit us at ${hora}:${min}`
+
+if (hora >=0 && hora<5) {
+  //madrugada
+  picture.src = `noite.png`
+  document.body.style.background ='#515154'
+} 
+if (hora >=5 && hora<12) {
+  //dia
+  picture.src = "manha.png"
+  document.body.style.background ='#e2cd9f'
+} 
+if (hora >=12 && hora<18) {
+  //tarde
+  picture.src = "tarde.png"
+  document.body.style.background ='#b9846f'
+} 
+if (hora >=18 && hora<=23) {
+  //noite
+  picture.src = "noite.png"
+  document.body.style.background ='#515154'
+} 
+
+
+// Timer
+
+var timenow = setInterval(myTimer);
     function myTimer() {
-        var d = new Date(), displayDate;
+        var time = new Date(), displayDate;
        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-          displayDate = d.toLocaleTimeString('pt-BR');
+          displayDate = time.toLocaleTimeString('pt-Br');
        } else {
-          displayDate = d.toLocaleTimeString('pt-BR', {timeZone: 'America/sao_paulo'});
+          displayDate = time.toLocaleTimeString('pt-Br', {timeZone: 'America/Sao_Paulo'});
        }
-          document.getElementById("horacerta").innerHTML ='Agora são exatamente: '+ displayDate;
+          document.getElementById("now").innerHTML ='Official Brazilian Time Now: '+ displayDate;
     }
 
+// Lot e Long
 
-// add o 0 se o minuto for menor que 10
-if (hora2 <10){
-    hora2 = '0' + hora2
+var coord = document.getElementById("coords");
+var button = document.getElementById('button').addEventListener('click', getLocation)
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    coord.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
-
-mensagem.innerHTML = `Olá, você nos visitou: ${hora} horas e ${hora2} minutos.`
-
-if (hora >4 && hora <12){
-    imagem.src="manha.png"
-    document.body.style.background = "#af6c72"
-    titulo.innerHTML = `<h1>Bom dia! </h1>`
-} else if (hora >=12 && hora <18) {
-    imagem.src="tarde.png"
-    document.body.style.background = "#a2d8fb"
-    titulo.innerHTML = `<h1> Boa tarde!</h1>`
-} else {
-    imagem.src="noite.png"
-    document.body.style.background = "#081d20"
-    titulo.innerHTML = `<h1> Boa noite!</h1>`
-}
+function showPosition(position) {
+  coord.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
 }
